75d48cbcfa7950fc5d9a8de6f656a2dc539d0375
Eid Osama <136953889+eid-osama@users.noreply.github.com>
2025-06-05 05:26:44 +0300

    Update model.py

--- utils/model.py
+++ utils/model.py
@@ -72,7 +72,7 @@ def get_vocoder(config, device):
         vocoder.mel2wav.eval()
         vocoder.mel2wav.to(device)
     elif name == "HiFi-GAN":
-        with open("hifigan/config.json", "r") as f:
+        with open("/kaggle/working/Arabic-TTS-kaggle-modfication/hifigan/config.json", "r") as f:
             config = json.load(f)
         config = hifigan.AttrDict(config)
         vocoder = hifigan.Generator(config)
